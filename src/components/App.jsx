4fe2b29208b9b44d0242b3ae9121b0f89434c9f0
OxanaDemova <93287016+OxanaDemova@users.noreply.github.com>
2022-07-28 23:05:37 +0300

    add input markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      
-    </div>
-  );
+import { Component } from "react";
+import { Container } from "./Container/Container";
+import { Phonebook } from "./Phonebook/Phonebook";
+
+
+export class App extends Component {
+  state = {
+  contacts: [],
+  name: ''
+  }
+  
+  render() {
+    return (
+      <Container>
+        <Phonebook
+        title={'Phonebook'}/>
+      </Container>
+
+    );
+  }
+
 };
