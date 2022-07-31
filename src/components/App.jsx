fc818d6ddd605b1c7dd1f7dbe7abc11aa8f00fce
OxanaDemova <93287016+OxanaDemova@users.noreply.github.com>
2022-07-31 19:31:16 +0300

    change label format

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,35 @@
 import { Component } from "react";
 import { nanoid } from "nanoid";
 import { Container } from "./Container/Container";
-import { Form } from "./Form/Form";
-import { ContactsList } from "./ContactsList/ContactsList";
+import { ContactForm } from "./ContactForm/ContactForm";
+import { ContactList } from "./ContactList/ContactList";
 
 export class App extends Component {
   state = {
-  contacts: [],
-  name: ''
+    contacts: [],
+    filter: ''
+
   }
 
-  nameId = nanoid();
+  formSubmitHandler = data => {
+    console.log(data);
+  }
   
   render() {
     return (
       <Container>
-        <Form
-           />
+        <h1>Phonebook</h1>
+        <ContactForm
+          onSubmit={this.formSubmitHandler}
+        />
+        <h2>Contacts</h2>
         {/* <ContactsList
           title={'Contacts'}
           id={this.nameId} /> */}
         
       </Container>
 
-    );
+    )
   }
 
-};
+}
